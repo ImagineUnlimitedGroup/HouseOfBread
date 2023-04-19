4ffe180cad8330f048d8ec9f3cccfcef2c99c59d
ImagineUnlimitedGroup <imagineunlimitedgroup@gmail.com>
2023-04-18 19:57:50 -0600

    Change Header

--- js/main.js
+++ js/main.js
@@ -10,37 +10,45 @@ const body = document.querySelector("body")
 const header = document.querySelector(".header")
 const pages = document.querySelectorAll(".menu__link")
 
-burger.onclick = function(){
-	HeaderToggleClasses()
-}
- 
-// document.addEventListener( 'click', (e) => {
-// 	const withinBoundaries = e.composedPath().includes(header);
- 
-// 	if ( ! withinBoundaries ) {
-// 		HeaderToggleClasses()
-// 	}
-// })
-
-pages.forEach(page => (
-	page.onclick = function(){
-		HeaderToggleClasses()
+const screenSizeCheck = () => {
+	let screenWidth = document.documentElement.clientWidth
+	if (screenWidth < 767) {
+		burger.onclick = function(){
+			HeaderToggleClasses()
+		}
+		
+		
+		document.addEventListener( 'click', (e) => {
+			const withinBoundaries = e.composedPath().includes(header);
+		
+			if ( ! withinBoundaries ) {
+				HeaderToggleClasses()
+			}
+		})
+	
+		pages.forEach(page => (
+			page.onclick = function(){
+				HeaderToggleClasses()
+			}
+		)) 
+	
+	
+		const HeaderToggleClasses = () => {
+			// ab = active burger
+				body.classList.toggle("lock");
+				burger.classList.toggle("ab");
+				header.classList.toggle("ab");
+				app.classList.toggle("ab");
+				bigContact.classList.toggle("ab");
+				logo.classList.toggle("ab");
+				menu.classList.toggle("ab");
+				container.classList.toggle("ab");
+				contacts.classList.toggle("ab");
+		}
 	}
-)) 
-
-
-const HeaderToggleClasses = () => {
-	// ab = active burger
-		body.classList.toggle("lock");
-		burger.classList.toggle("ab");
-		header.classList.toggle("ab");
-		app.classList.toggle("ab");
-		bigContact.classList.toggle("ab");
-		logo.classList.toggle("ab");
-		menu.classList.toggle("ab");
-		container.classList.toggle("ab");
-		contacts.classList.toggle("ab");
 }
+screenSizeCheck()
+
 
 function initMap() {
 	var uluru = {lat: 39.67955763123156, lng: -104.87620813150541};
