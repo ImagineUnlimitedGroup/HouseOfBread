66717a6ecad700278a0276aea9c9f9dea04986fa
ImagineUnlimitedGroup <imagineunlimitedgroup@gmail.com>
2023-04-29 13:44:31 -0600

    About Us

---
+++ js/aboutUs.js
@@ -0,0 +1,41 @@
+const photosList = document.getElementById('aboutUS-slider__list')
+
+let photosAmount = 3
+let sliderPerPage = 3
+
+// Генерируем все картинки
+for (let i = 1; i <= photosAmount; i++) {
+	let element = document.createElement('li')
+
+	element.classList.add('splide__slide');
+	element.classList.add('aboutUS__item');
+
+	imgPath = './img/icons/logo.png'
+	
+	let template = `<img src="./img/aboutUs/${i}.png" alt="${i}" onError="this.src='${imgPath}'">`
+	
+	element.innerHTML = template
+	photosList.appendChild(element)
+}
+
+// Инициализация слайдера для галереи
+document.addEventListener('DOMContentLoaded', function () {
+	new Splide('#aboutUS-slider', {
+		perPage: sliderPerPage,
+		perMove: 1,
+		type : 'loop',
+		gap: '10px',
+		breakpoints: {
+			991.98: {
+				perPage: 2,
+				gap: '10px',
+			},
+			767.98: {
+				perPage: 2,
+			},
+			650: {
+				perPage: 1,
+			}
+		},
+	}).mount();
+});
