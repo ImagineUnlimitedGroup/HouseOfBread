4c6c34546f39ab4b7e8e7a489ad387f31e3e557a
ImagineUnlimitedGroup <imagineunlimitedgroup@gmail.com>
2023-04-29 15:51:21 -0600

    4

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ const categoriesList = document.querySelector('.categories-menu__list')
 
 // Массив с данными всех категорий меню
 let categoriesArr = [
-	{title: 'MADE TO ORDER', imgPath: './img/menu/mar/steak.svg', link: './categoriesHtml/madeToOrder.html'},
+	{title: 'MADE TO ORDER', imgPath: './img/menu/madeToOrder/steak.svg', link: './categoriesHtml/madeToOrder.html'},
 	{title: 'SALADS', imgPath: './img/menu/salads/salad.svg', link: './categoriesHtml/salads.html'},
 	{title: 'PASTRIES', imgPath: './img/menu/pastries/pastries.svg', link: './categoriesHtml/pastries.html'},
 	{title: 'CAKES', imgPath: './img/menu/cakes/cakes.svg', link: './categoriesHtml/cakes.html'},
